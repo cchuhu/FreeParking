d62aca05f5ca31ef546f1de0c8ced0b32f600335
zhangwen <646689983@qq.com>
2016-07-07 09:57:47 +0800

    修改输出

--- app/src/main/java/huhu/com/freeparking/Util/NetworkState.java
+++ app/src/main/java/huhu/com/freeparking/Util/NetworkState.java
@@ -27,8 +27,6 @@ public class NetworkState {
 
             if (networkInfo != null && networkInfo.length > 0) {
                 for (int i = 0; i < networkInfo.length; i++) {
-                    System.out.println(i + "===状态===" + networkInfo[i].getState());
-                    System.out.println(i + "===类型===" + networkInfo[i].getTypeName());
                     // 判断当前网络状态是否为连接状态
                     if (networkInfo[i].getState() == NetworkInfo.State.CONNECTED) {
                         return true;
