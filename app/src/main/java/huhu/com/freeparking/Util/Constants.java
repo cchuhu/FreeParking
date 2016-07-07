724b5fcefe6c5838818cd02684c1be367c928ec9
zhangwen <646689983@qq.com>
2016-07-07 16:55:58 +0800

    添加常用常量

--- app/src/main/java/huhu/com/freeparking/Util/Constants.java
+++ app/src/main/java/huhu/com/freeparking/Util/Constants.java
@@ -7,5 +7,10 @@ package huhu.com.freeparking.Util;
 public class Constants {
     //停车场通车数
     public static String CAR_COUNT = "0";
-    //
+    //管理员账号
+    public static String Manager_Account;
+    //管理员姓名
+    public static String Manager_Name;
+    //管理员头像图片
+    public static String Manager_Icon;
 }
