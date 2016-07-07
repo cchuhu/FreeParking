2cef0b0de1ad134c2352f8541bd00a6dda163d0c
zhangwen <646689983@qq.com>
2016-07-07 15:47:51 +0800

    存储常量数据

---
+++ app/src/main/java/huhu/com/freeparking/Util/Constants.java
@@ -0,0 +1,11 @@
+package huhu.com.freeparking.Util;
+
+/**
+ * Created by Huhu on 7/7/16.
+ * 存放全局常量数据
+ */
+public class Constants {
+    //停车场通车数
+    public static String CAR_COUNT = "0";
+    //
+}
