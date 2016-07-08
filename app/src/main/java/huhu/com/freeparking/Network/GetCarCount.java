815c8dd62c47310b37f47d24d098955388caaf4d
zhangwen <646689983@qq.com>
2016-07-08 10:58:21 +0800

    添加获取停车数量接口

---
+++ app/src/main/java/huhu/com/freeparking/Network/GetCarCount.java
@@ -0,0 +1,32 @@
+package huhu.com.freeparking.Network;
+
+import huhu.com.freeparking.Util.HttpMethod;
+import huhu.com.freeparking.Util.NetConnection;
+
+/**
+ * Created by Huhu on 7/8/16.
+ * 获取停车数量的工具类
+ */
+public class GetCarCount {
+    public GetCarCount(String url, final getSuccess getSuccess, final getFailed getFailed) {
+        new NetConnection(url, HttpMethod.POST, new NetConnection.SuccessCallback() {
+            @Override
+            public void onSuccess(String rt) {
+                getSuccess.onSuccess(rt);
+            }
+        }, new NetConnection.FailCallback() {
+            @Override
+            public void onFail() {
+                getFailed.onFailed();
+            }
+        });
+    }
+
+    public interface getSuccess {
+        void onSuccess(String result);
+    }
+
+    public interface getFailed {
+        void onFailed();
+    }
+}
