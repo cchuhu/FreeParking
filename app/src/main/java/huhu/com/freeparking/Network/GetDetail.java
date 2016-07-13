ac6c8ae81523d6308ffd6d36cd426a8b6b51dd57
zhangwen <646689983@qq.com>
2016-07-13 16:20:33 +0800

    添加获取列表信息接口

---
+++ app/src/main/java/huhu/com/freeparking/Network/GetDetail.java
@@ -0,0 +1,32 @@
+package huhu.com.freeparking.Network;
+
+import huhu.com.freeparking.Util.HttpMethod;
+import huhu.com.freeparking.Util.NetConnection;
+
+/**
+ * Created by Huhu on 7/13/16.
+ * 获取商家与停车券数量对应关系
+ */
+public class GetDetail {
+    public GetDetail(String url, final getSuccess getSuccess, final getFailed getFailed) {
+        new NetConnection(url, HttpMethod.POST, new NetConnection.SuccessCallback() {
+            @Override
+            public void onSuccess(String rt) {
+                getSuccess.onSuccess(rt);
+            }
+        }, new NetConnection.FailCallback() {
+            @Override
+            public void onFail() {
+                getFailed.onFailed();
+            }
+        });
+    }
+
+    public interface getSuccess {
+        void onSuccess(String result);
+    }
+
+    public interface getFailed {
+        void onFailed();
+    }
+}
