9cc8ab812667456ca2f31e869691931b74a1b936
zhangwen <646689983@qq.com>
2016-07-13 16:19:32 +0800

    添加bean

---
+++ app/src/main/java/huhu/com/freeparking/Widget/TicketBean.java
@@ -0,0 +1,26 @@
+package huhu.com.freeparking.Widget;
+
+/**
+ * Created by Huhu on 7/13/16.
+ * 商家与停车券的对应信息
+ */
+public class TicketBean {
+    //商家名称，商家对应的停车券
+    private String sname, snum;
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getSnum() {
+        return snum;
+    }
+
+    public void setSnum(String snum) {
+        this.snum = snum;
+    }
+}
