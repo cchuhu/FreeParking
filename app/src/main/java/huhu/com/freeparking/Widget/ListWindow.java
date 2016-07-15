2f051b00452ef854f87652976aca134f6d406be1
zhangwen <646689983@qq.com>
2016-07-15 10:51:51 +0800

    消费信息弹窗

--- app/src/main/java/huhu/com/freeparking/Widget/ListWindow.java
+++ app/src/main/java/huhu/com/freeparking/Widget/ListWindow.java
@@ -3,7 +3,13 @@ package huhu.com.freeparking.Widget;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import huhu.com.freeparking.R;
 
@@ -11,27 +17,70 @@ import huhu.com.freeparking.R;
  * Created by Huhu on 7/13/16.
  * 显示商家与停车券对应关系的窗口
  */
-public class ListWindow extends PopupWindow{
+public class ListWindow extends PopupWindow {
     //弹窗视图
     private View view;
+    //数据信息
+    private ArrayList<TicketBean> arrayList;
+    //列表视图
+    private ListView listView;
 
 
     /**
      * 构造函数
      */
-    public ListWindow(Context context, int width, int height) {
-        view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.popwindow, null);
+    public ListWindow(final Context context, final ArrayList<TicketBean> arrayList, int width, int height) {
+        view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.listwindow, null);
+        this.arrayList = arrayList;
+        //初始化视图
+        listView = (ListView) view.findViewById(R.id.lv_detail);
+        listView.setAdapter(new LotlistAdapter(context, arrayList));
         setHeight(height);
         setWidth(width);
         setContentView(view);
-        view.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                if (ListWindow.this.isShowing()) {
-                    dismiss();
-                }
-            }
-        });
     }
+
+
+    /**
+     * 适配器类
+     */
+    class LotlistAdapter extends BaseAdapter {
+        private ArrayList<TicketBean> arrayList;
+        private LayoutInflater inflater;
+
+        public LotlistAdapter(Context context, ArrayList<TicketBean> arrayList) {
+            this.arrayList = arrayList;
+            inflater = LayoutInflater.from(context);
+
+        }
+
+        @Override
+        public int getCount() {
+            return arrayList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return arrayList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = inflater.inflate(R.layout.parkiten, null);
+            }
+            TextView sname = (TextView) view.findViewById(R.id.tv_sname);
+            TextView snum = (TextView) view.findViewById(R.id.tv_snum);
+            sname.setText(arrayList.get(i).getSname());
+            snum.setText(arrayList.get(i).getSnum());
+            return view;
+        }
+    }
+
 }
