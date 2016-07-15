60bd4d84b5cccd719f69f2da090f971859d597b5
zhangwen <646689983@qq.com>
2016-07-15 10:52:14 +0800

    停车场信息bean

---
+++ app/src/main/java/huhu/com/freeparking/Widget/ParkLotBean.java
@@ -0,0 +1,24 @@
+package huhu.com.freeparking.Widget;
+
+/**
+ * Created by Huhu on 7/15/16.
+ */
+public class ParkLotBean {
+    private String lot_id,lot_address;
+
+    public String getLot_id() {
+        return lot_id;
+    }
+
+    public void setLot_id(String lot_id) {
+        this.lot_id = lot_id;
+    }
+
+    public String getLot_address() {
+        return lot_address;
+    }
+
+    public void setLot_address(String lot_address) {
+        this.lot_address = lot_address;
+    }
+}
